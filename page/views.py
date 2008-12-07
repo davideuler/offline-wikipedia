a412519c88572cfb8369531b74eece4e5736b015
choudhary.shantanu <choudhary.shantanu@97e8de00-bf93-11dd-ad2b-77eeadef73a9>
2008-12-07 07:29:57 +0000

    updated views.py for better handling of links having more then one-words, still need to handle types like eng-Wiki, they will return no page :(

--- page/views.py
+++ page/views.py
@@ -5,20 +5,23 @@ from django.http import HttpResponse
 
 def req_handler(request, fname):		
 	#please replase xxx and others with proper parameters to make it working.
-	con = psycopg2.connect("dbname='xxx' user='yyy' password='zzz'")
+	con = psycopg2.connect("dbname='baali' user='baali' password='pong'")
 	cur = con.cursor()	
 	page = fname.split('/')
-	page[-1] = page[-1].capitalize()
-	cur.execute('select * from enwiki_db where name=%s',[page[-1]])
+	print page[-1]
+	#to make first letter capital, while in links it is smaller but in table/xml, it is always Capital
+	link = page[-1][0].upper()+page[-1][1:len(page[-1])]		
+	print link
+	cur.execute('select * from enwiki_db where name=%s',[link])
 	r = cur.fetchone()	
 	if r != None:
-		print r[0]
+		print r[0], r[1], r[2], r[3]
 		#print len(r)
 		Obj = class_con.Xml_Html(r[1],r[2],r[3])
-		while Obj.flag_red == 1:
-			print Obj.link_red			
-			Obj.link_red = Obj.link_red.capitalize()			
+		while Obj.flag_red == 1:									
+			Obj.link_red = Obj.link_red[0].upper()+Obj.link_red[1:len(Obj.link_red)]			
 			if r[0] == Obj.link_red:
+				print Obj.link_red
 				#self redirecting loop..........
 				return HttpResponse('Page redirecting to Itself...')
 				break
