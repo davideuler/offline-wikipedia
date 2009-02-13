1431c6090bb2e76321fa7817b5bf30ee02b51049
choudhary.shantanu <choudhary.shantanu@97e8de00-bf93-11dd-ad2b-77eeadef73a9>
2009-02-13 12:48:14 +0000

    changed urls.py for first page view, then using sqlite db rather then postgress

--- trunk/page/views.py
+++ trunk/page/views.py
@@ -1,18 +1,44 @@
 # Create your views here.
 import class_con
-import psycopg2
+import sqlite3
 from django.http import HttpResponse
+
+def first(request):
+	front_page = ''
+	con = sqlite3.connect('english-xml-db')
+	cur = con.cursor()
+	cur.execute('select name from articles')
+	template = open('data/en_template').read().decode('utf-8')			
+	front_page = template[0:template.find('<title>')]
+	front_page += '<title>List of all Articles</titile>\n'
+	front_page += template[template.find('<title>')+7:template.find('<h1 class="firstHeading">')+len('<h1 class="firstHeading">')]
+	front_page += 'Article List</h1>'
+	front_page += template[template.find('<div id="bodyContent">'):template.find('<!-- start content -->')+len('<!-- start content -->')]+'\n'
+	counter = 0
+	front_page += '<table>\n<tr>\n'
+	for row in cur:
+		if counter == 3:
+			front_page += '</tr>\n<tr>\n'
+			counter = 0
+		front_page += '<td><a href="/wiki/'+row[0]+'/">'+row[0]+'</a></td>\n'
+		counter += 1
+	if counter != 0:
+		front_page += '</tr>\n</table>'
+	else:
+		front_page += '</table>'		
+	front_page += template[template.find('<!-- start content -->')+len('<!-- start content -->'):len(template)]
+	return HttpResponse(front_page)
 
 def req_handler(request, fname):		
 	#please replase xxx and others with proper parameters to make it working.
-	con = psycopg2.connect("dbname='xxx' user='xxx' password='xxx'")
+	con = sqlite3.connect('english-xml-db')
 	cur = con.cursor()	
 	page = fname.split('/')
 	print page[-1]
 	#to make first letter capital, while in links it is smaller but in table/xml, it is always Capital
 	link = page[-1][0].upper()+page[-1][1:len(page[-1])]		
 	print link
-	cur.execute('select * from enwiki_db where name=%s',[link])
+	cur.execute('select * from articles where name = (?)',[link])
 	r = cur.fetchone()	
 	if r != None:
 		print r[0], r[1], r[2], r[3]
@@ -25,7 +51,7 @@ def req_handler(request, fname):
 				#self redirecting loop..........
 				return HttpResponse('Page redirecting to Itself...')
 				break
-			cur.execute('select * from enwiki_db where name=%s',[Obj.link_red])
+			cur.execute('select * from articles where name = (?)',[Obj.link_red])
 			r = cur.fetchone()
 			if r != None:
 				Obj = class_con.Xml_Html(r[1],r[2],r[3])
