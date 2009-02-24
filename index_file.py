1dcbcad331f1d10e287450cf13c47e955616610d
choudhary.shantanu <choudhary.shantanu@97e8de00-bf93-11dd-ad2b-77eeadef73a9>
2009-02-24 15:19:40 +0000

    added files for creatinf csv file, and dividing single csv to seprate files based on the first character

---
+++ index_file.py
@@ -0,0 +1,72 @@
+#File to create index table named enwiki_db with dbname = ...... , from fragmented enwiki bz2 files.
+import bz2
+from elementtree import ElementTree
+import os
+import csv
+
+#this variable stores the location of your fragmented wiki-dumps.
+#for me the files are of name rec0xxxxxenwiki-20080724-pages-articles.xml.bz2
+#name_start will have starting part of location, coming before rec, replace xxx with that
+#name_start = '/home/baali/ks/rec'
+name_start = 'data/xml_blocks/rec'
+#here xxx will correspond to date of dumps you have downladed, for me xxx was 20080724, please replace it accordingly.
+#name_end = 'hiwiki-20080616-pages-articles.xml.bz2'
+name_end = 'enwiki-20080724-pages-articles.xml.bz2'
+num = 1
+fname = '%(fstart)s%(fnum)05d%(fend)s' %{'fstart':name_start,'fnum':num,'fend':name_end}
+block_file = open(fname,'rb')
+data = bz2.decompress(block_file.read())
+pos = 0
+#csv file with all details
+archiveWriter = csv.writer(open('archive.csv','wb'))
+#check for first article in the chunk, create a folder with first alphabet of that article, move whole chunk to it.... consider it to e secondary
+#create separate tables for different articles, like table with first alphabet, and numerical table, and create index for each table.
+archiveWriter = csv.writer(open('archive.csv','wb'))
+
+while True:		
+	beg = data.find('<page>',pos)	
+
+	if beg == -1:
+		#perfect end in previous block
+		block_file.close()
+		num += 1
+		fname = '%(fstart)s%(fnum)05d%(fend)s' %{'fstart':name_start,'fnum':num,'fend':name_end}
+		print fname
+		if os.path.isfile(fname):
+			block_file = open(fname,'rb')	
+		else:
+			break
+		data = bz2.decompress(block_file.read())
+		beg = data.find('<page>',pos)		
+		end = data.find('</page>',beg)
+		block = data[beg:end]
+
+	elif beg != -1 and data.find('</page>',beg) == -1:
+		#there is a article spanning in two blocks
+		block = data[beg:len(data)]
+		block_file.close()
+		num += 1
+		fname = '%(fstart)s%(fnum)05d%(fend)s' %{'fstart':name_start,'fnum':num,'fend':name_end}
+		print fname
+		if os.path.isfile(fname):
+			block_file = open(fname,'rb')	
+		else:
+			break
+		data = bz2.decompress(block_file.read())
+		end = data.find('</page>')
+		block += data[0:end]
+	else:
+		end = data.find('</page>',beg)
+		#normal case article starts and ends in one block
+		block = data[beg:end]		
+	pos = end+7
+	title_start = block.find('<title>')			
+	title = block[title_start+7:block.find('</title>',title_start)].decode('utf-8')
+	#fake entry
+	if len(title)==0:
+		continue
+	#elif (title[0] == )
+	archiveWriter.writerow([title.encode('utf-8'),num,beg,end])			
+
+#done.....	
+#archiveWriter.close()
